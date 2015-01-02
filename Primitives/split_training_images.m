d5a3c97f425b9dd4dc9628cce8b91a816b1552e6
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-01-01 20:21:21 -0800

    split training and test images

---
+++ Primitives/split_training_images.m
@@ -0,0 +1,60 @@
+function split_training_images
+
+root_dir = '/home/yuxiang/Projects/3DEverything/Primitives';
+azimuth = 0:15:345;
+elevation = 0:15:90;
+yaw = 0:30:330;
+per_test = 0.2;
+
+% read class names
+fid = fopen('classes.txt', 'r');
+C = textscan(fid, '%s');
+classes = C{1};
+fclose(fid);
+N = numel(classes);
+
+% output files
+fid_train_images = fopen('train_images.txt', 'w');
+fid_train_depths = fopen('train_depths.txt', 'w');
+fid_test_images = fopen('test_images.txt', 'w');
+fid_test_depths = fopen('test_depths.txt', 'w');
+
+% for each class
+for c = 1:N
+    cls = classes{c};
+    disp(cls);
+    files = dir(sprintf('CAD/%s/*.obj', cls));
+    % number of CAD models
+    num = numel(files);
+    
+    % number of CAD models for testing
+    num_test = max(round(num * per_test), 1);
+    num_train = num - num_test;
+    
+    outdir = sprintf('%s/training_images/%s', root_dir, cls);
+    
+    % for each CAD model
+    for i = 1:num
+        % render for each viewpoint
+        for a = azimuth
+            for e = elevation
+                for y = yaw
+                    filename_image = sprintf('%s/cad%02d_a%03d_e%02d_y%03d_image.jpg', outdir, i, a, e, y);
+                    filename_depth = sprintf('%s/cad%02d_a%03d_e%02d_y%03d_depth.jpg', outdir, i, a, e, y);
+                    if i <= num_train
+                        fprintf(fid_train_images, '%s\n', filename_image);
+                        fprintf(fid_train_depths, '%s\n', filename_depth);
+                    else
+                        fprintf(fid_test_images, '%s\n', filename_image);
+                        fprintf(fid_test_depths, '%s\n', filename_depth);
+                    end
+                end
+            end
+        end
+    end
+end
+
+fclose(fid_train_images);
+fclose(fid_train_depths);
+fclose(fid_test_images);
+fclose(fid_test_depths);
