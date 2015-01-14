d877605e6ba4f01f5e38c94fefde3ddd13df8ae2
Yu Xiang <yuxiang@napoli102.Stanford.EDU>
2015-01-14 14:32:02 -0800

    create image mean

---
+++ CNN/make_imageset_mean.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# Compute the mean image from the training lmdb
+
+CAFFE_ROOT=/opt/caffe-master
+
+$CAFFE_ROOT/build/tools/compute_image_mean train_images_lmdb imageset_mean.binaryproto
+
+echo "Done."
